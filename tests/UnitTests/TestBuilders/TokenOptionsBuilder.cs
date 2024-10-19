f3a23f0fb7e832982429433944eccbb74fb02c31
joaosouzaaa <joaoasouza982@gmail.com>
2024-10-19 20:29:16 -0300

    Tests: TokenOptionsValidator unit tests.

---
+++ tests/UnitTests/TestBuilders/TokenOptionsBuilder.cs
@@ -0,0 +1,51 @@
+﻿using OptionsPatternValidation.API.Options;
+
+namespace UnitTests.TestBuilders;
+
+public sealed class TokenOptionsBuilder
+{
+    private string _key = "10003d68b1123asa1asdadasd231asd7d";
+    private string _issuer = "random";
+    private string _audience = "test";
+    private int _expirationTimeInMinutes = 5;
+
+    public static TokenOptionsBuilder NewObject() =>
+        new();
+
+    public TokenOptions OptionsBuild() =>
+        new()
+        {
+            Key = _key,
+            Issuer = _issuer,
+            Audience = _audience,
+            ExpirationTimeInMinutes = _expirationTimeInMinutes
+        };
+
+    public TokenOptionsBuilder WithKey(string key)
+    {
+        _key = key;
+
+        return this;
+    }
+
+    public TokenOptionsBuilder WithIssuer(string issuer)
+    {
+        _issuer = issuer;
+
+        return this;
+    }
+
+    public TokenOptionsBuilder WithAudience(string audience)
+    {
+        _audience = audience;
+
+        return this;
+    }
+
+    public TokenOptionsBuilder WithExpirationTimeInMinutes(int expirationTimeInMinutes)
+    {
+        _expirationTimeInMinutes = expirationTimeInMinutes;
+
+        return this;
+    }
+}
---
+++ tests/UnitTests/ValidatorsTests/TokenOptionsValidatorTests.cs
@@ -0,0 +1,88 @@
+﻿using OptionsPatternValidation.API.Validators;
+using UnitTests.TestBuilders;
+
+namespace UnitTests.ValidatorsTests;
+
+public sealed class TokenOptionsValidatorTests
+{
+    private readonly TokenOptionsValidator _tokenOptionsValidator;
+
+    public TokenOptionsValidatorTests()
+    {
+        _tokenOptionsValidator = new TokenOptionsValidator();
+    }
+
+    [Fact]
+    public void Validate_SuccessfulScenario_ReturnsTrue()
+    {
+        // A
+        var tokenOptions = TokenOptionsBuilder.NewObject().OptionsBuild();
+
+        // A
+        var validationResult = _tokenOptionsValidator.Validate(tokenOptions);
+
+        // A
+        Assert.True(validationResult.IsValid);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("test")]
+    [InlineData("random")]
+    public void Validate_InvalidKey_ReturnsFalse(string key)
+    {
+        // A
+        var tokenOptionsWithInvalidKey = TokenOptionsBuilder.NewObject().WithKey(key).OptionsBuild();
+
+        // A
+        var validationResult = _tokenOptionsValidator.Validate(tokenOptionsWithInvalidKey);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    [Fact]
+    public void Validate_InvalidKey_ReturnsIssuer()
+    {
+        // A
+        var tokenOptionsWithInvalidIssuer = TokenOptionsBuilder.NewObject().WithIssuer(null!).OptionsBuild();
+
+        // A
+        var validationResult = _tokenOptionsValidator.Validate(tokenOptionsWithInvalidIssuer);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    [Fact]
+    public void Validate_InvalidAudience_ReturnsFalse()
+    {
+        // A
+        var tokenOptionsWithInvalidAudience = TokenOptionsBuilder.NewObject().WithAudience(string.Empty).OptionsBuild();
+
+        // A
+        var validationResult = _tokenOptionsValidator.Validate(tokenOptionsWithInvalidAudience);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(4)]
+    [InlineData(-2)]
+    public void Validate_InvalidExpirationTimeInMinutes_ReturnsFalse(int expirationTimeInMinutes)
+    {
+        // A
+        var tokenOptionsWithInvalidExpirationTimeInMinutes = TokenOptionsBuilder
+            .NewObject()
+            .WithExpirationTimeInMinutes(expirationTimeInMinutes)
+            .OptionsBuild();
+
+        // A
+        var validationResult = _tokenOptionsValidator.Validate(tokenOptionsWithInvalidExpirationTimeInMinutes);
+
+        // A
+        Assert.False(validationResult.IsValid);
+    }
+}
